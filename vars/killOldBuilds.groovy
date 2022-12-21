67a97c1e7b8d1b9609dc43e738bcb6279be24184
aligredo <aliahmedismail37@gmail.com>
2022-12-21 15:53:12 +0200

    modified killOldBuild script

--- vars/killOldBuilds.groovy
+++ vars/killOldBuilds.groovy
@@ -3,6 +3,11 @@ import jenkins.model.CauseOfInterruption.UserInterruption
 
 def call() {
   while(currentBuild.rawBuild.getPreviousBuildInProgress() != null) {
-    currentBuild.rawBuild.getPreviousBuildInProgress().doKill()
+    echo ">> Aborting older build #${currentBuild.rawBuild.getPreviousBuildInProgress().number}"
+    def executor = currentBuild.rawBuild.getPreviousBuildInProgress().previousBuild.getExecutor()
+    executor.interrupt(Result.ABORTED, new UserInterruption(
+                    "Aborted by newer build #${currentBuild.number}"
+                ))
+    currentBuild.rawBuild.getPreviousBuildInProgress().doStop()
   }
 }
