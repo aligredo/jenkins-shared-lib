6b6e505d256fd15d17509ee624f125598ca963ed
aligredo <aliahmedismail37@gmail.com>
2022-12-21 16:21:20 +0200

    modified killOldBuild script

--- vars/killOldBuilds.groovy
+++ vars/killOldBuilds.groovy
@@ -3,6 +3,8 @@ import jenkins.model.CauseOfInterruption.UserInterruption
 
 def call() {
   while(currentBuild.rawBuild.getPreviousBuildInProgress() != null) {
+    Run = currentBuild.rawBuild.getPreviousBuildInProgress()
+    sh "echo Abortig build #${olderBuild.number}"
     currentBuild.rawBuild.getPreviousBuildInProgress().doStop()
   }
 }
